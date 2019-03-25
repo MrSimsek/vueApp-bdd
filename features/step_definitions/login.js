8df2e8abd90070509a9dbac9d53b0c165ad70180
mrsimsek <denizsimsek93@gmail.com>
2019-03-25 16:22:05 +0300

    Add comments

--- features/step_definitions/login.js
+++ features/step_definitions/login.js
@@ -9,19 +9,23 @@ Given('a user exists with username {string} and password {string}', async (usern
     await UI.fillInFormField('username', username);
     await UI.fillInFormField('password', password);
     await UI.pressButton('register');
+    // services: check if user is in users object
     return await UI.shouldBeOnPage('login');
 });
 
 Given('I provide {string} with {string}', UI.fillInFormField);
 
 When('the user is logging in', async () => {
+    // UI: check if form fields are empty
     return await UI.pressButton('login');
 });
 
 Then('the user should be logged in', async () => {
+    // UI: check the page has user.firstName
     return await services.checkLocalStorageHasUser('denizsimsek');
 });
 
 Then('the user should not be logged in', async () => {
+    // UI: check if there is an error message on the page
     return await services.checkLocalStorageHasNoUser();
 });
